087bf601abba8ffb7ce01d060763b1465cb5b25f
reptiliaani <aleksi.siponen@gmail.com>
2019-06-04 10:08:45 +0300

    poistellaan

--- osa2/puhelinluettelo/src/components/person.js
+++
@@ -1,9 +0,0 @@
-import react from 'React'
-import React, { useState } from 'react'
-
-const rows = () => personsToShow.map( person => {
-    return(
-      <li key={person.id}>{person.name} {person.number}</li>
-    )
-})
-export default Person
